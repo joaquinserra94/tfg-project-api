c4be6c7085de939e7a04dee81d3bad7e87743ea2
Joaquin <joaquinserra94@gmail.com>
2026-03-09 22:38:56 +0100

    feat: configure alembic and initial projects migration

---
+++ app/db/base_class.py
@@ -0,0 +1,4 @@
+from app.db.base import Base
+from app.models import Project
+
+__all__ = ["Base", "Project"]
---
+++ app/models/project.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, DateTime, Integer, String, func
+
+from app.db.base import Base
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(150), nullable=False)
+    description = Column(String(500), nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
